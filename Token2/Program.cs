748b84b8df196d95e9bf021dc76735ae07201809
Victor Shi <62452282+victorshi29@users.noreply.github.com>
2020-07-08 03:22:17 -0400

    Add files via upload

---
+++ Token2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlPath = "C:\\Users\\victo\\Desktop\\testfiles\\token2url.txt";
+            string headerPath = "C:\\Users\\victo\\Desktop\\testfiles\\token2header.txt";
+            string tokenURL = Methods.readFile(urlPath);
+            string requestHeaders = Methods.readFile(headerPath);
+                 
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+
+            HttpContent httpContent = new StringContent(requestHeaders, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            string message = (Methods.postRequest(client, tokenURL, httpContent)).Result;
+            Console.WriteLine(message);
+
+            Token token = JsonSerializer.Deserialize<Token>(message);
+            string jsonString = JsonSerializer.Serialize(token);
+            Console.WriteLine(jsonString);
+        }
+    }
